7bb17812e268b591005debe9389f0cc27489fb2e
JoahanC <joahan.castanedajaimes@gmail.com>
2022-06-17 17:17:43 -0700

    Created deck class with opening deck profile.

--- src/deck.py
+++ src/deck.py
@@ -1,3 +1,21 @@
 """
 This file establishes the deck class which simulates a standard card deck.
 """
+
+
+class Deck:
+
+
+    def __init__(self):
+        self.card_count = 52
+        self.cards = {}
+        
+        # setting up basic card profile
+        self.cards["King"] = 4
+        self.cards["Queen"] = 4
+        self.cards["Ace"] = 4
+
+
+
+    def cards():
+        
